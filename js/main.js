58befca3ab17155c44b94f40d7a8904799cfd859
HectorMtz22 <51431991+HectorMtz22@users.noreply.github.com>
2021-09-07 05:22:31 +0000

    Deploying to gh-pages from @ HectorMtz22/lang-pro-pia@af9918292f5b81a511761e587037f1c3d7eeca27 🚀

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-const sections = document.querySelectorAll(".sidebar section");
+const main = document.querySelector(".sidebar");
 const divs = document.querySelectorAll(".sidebar div");
 
 const CONSTANTS = {
-  personales: 'Personales',
-  tareas: 'Tareas',
-}
+  personales: "Personales",
+  tareas: "Tareas",
+};
 
 const toggle = (element, className) => {
   element.classList.toggle(className);
 };
 
 const toggleInvisible = (event) => {
-  const texto = event.path[0].innerText;
+  const texto = event.srcElement.innerText.trim();
   const [Personales, Tareas] = divs;
 
+  if (event.srcElement.tagName !== "A") {
+    return;
+  }
   if (texto === CONSTANTS.personales) {
     toggle(Personales, "toggleInvisible");
     setTimeout(() => toggle(divs[0], "invisible"), 1000);
@@ -22,6 +25,4 @@ const toggleInvisible = (event) => {
   }
 };
 
-sections.forEach((section) => {
-  section.addEventListener("click", toggleInvisible);
-});
+main.addEventListener("click", toggleInvisible);
