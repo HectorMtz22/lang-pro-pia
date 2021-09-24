099015ecac586733c583f5baee873bb7e769e47b
HectorMtz22 <hec.gre81@gmail.com>
2021-09-24 14:09:51 -0500

    Comentando toda la logica del menú

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,28 @@
-const main = document.querySelector(".sidebar");
-const divs = document.querySelectorAll(".sidebar div");
+// const main = document.querySelector(".sidebar");
+// const divs = document.querySelectorAll(".sidebar div");
 
-const CONSTANTS = {
-  personales: "Personales",
-  tareas: "Tareas",
-};
+// const CONSTANTS = {
+//   personales: "Personales",
+//   tareas: "Tareas",
+// };
 
-const toggle = (element, className) => {
-  element.classList.toggle(className);
-};
+// const toggle = (element, className) => {
+//   element.classList.toggle(className);
+// };
 
-const toggleInvisible = (event) => {
-  const texto = event.srcElement.innerText.trim();
-  const [Personales, Tareas] = divs;
+// // const toggleInvisible = (event) => {
+// //   const texto = event.srcElement.innerText.trim();
+// //   const [Personales, Tareas] = divs;
 
-  if (event.srcElement.tagName !== "A") {
-    return;
-  }
-  if (texto === CONSTANTS.personales) {
-    toggle(Personales, "toggleInvisible");
-    setTimeout(() => toggle(divs[0], "invisible"), 1000);
-  } else {
-    toggle(Tareas, "toggleInvisible");
-  }
-};
+// //   if (event.srcElement.tagName !== "A") {
+// //     return;
+// //   }
+// //   if (texto === CONSTANTS.personales) {
+// //     toggle(Personales, "toggleInvisible");
+// //     setTimeout(() => toggle(divs[0], "invisible"), 1000);
+// //   } else {
+// //     toggle(Tareas, "toggleInvisible");
+// //   }
+// // };
 
-main.addEventListener("click", toggleInvisible);
+// // main.addEventListener("click", toggleInvisible);
